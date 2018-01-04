f4ed7367e1e28fb97030d63644d3e161a4d61b45
iwate <iwatesoftware@live.jp>
2018-01-04 15:26:08 +0900

    fix dependencies

--- Aiplugs.CMS.Web/Models/JsonSearch/QParser.cs
+++ Aiplugs.CMS.Web/Models/JsonSearch/QParser.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Collections.Immutable;
 using Pidgin;
 using static Pidgin.Parser;
 using static Pidgin.Parser<char>;
@@ -10,7 +11,7 @@ namespace Aiplugs.CMS.Web.Models.JsonSearch
   {
     private static readonly Parser<char, char> LBracket = Char('[');
     private static readonly Parser<char, char> RBracket = Char(']');
-	private static readonly Parser<char, char> LParenthesis = Char('(');
+	  private static readonly Parser<char, char> LParenthesis = Char('(');
     private static readonly Parser<char, char> RParenthesis = Char(')');
     private static readonly Parser<char, char> DQ = Char('"');
     private static readonly Parser<char, char> SQ = Char('\'');
@@ -37,7 +38,7 @@ namespace Aiplugs.CMS.Web.Models.JsonSearch
                                                          from f in Digit.AtLeastOnceString()
                                                          select decimal.Parse($"{(sign.HasValue?sign.Value.ToString():"")}{d}{(dot.HasValue?".":"")}{f}");
     private static readonly Parser<char, IQuery> Num = Num1.Or(Num2).Select<IQuery>(n => new QNum(n));
-	private static readonly char[] StrIgnoreChars = new[]{',',' ','[',']'};
+	  private static readonly char[] StrIgnoreChars = new[]{',',' ','[',']'};
     private static readonly Parser<char, string> String1 = Token(c => !StrIgnoreChars.Contains(c)).ManyString().Between(SkipWhitespaces);
     private static readonly Parser<char, string> String2 = Token(c => c != '"').ManyString().Between(DQ);
     private static readonly Parser<char, string> String3 = Token(c => c != '\'').ManyString().Between(SQ);
@@ -46,22 +47,22 @@ namespace Aiplugs.CMS.Web.Models.JsonSearch
     private static readonly Parser<char, QConn> Conn = And.Or(Or).Select(o => new QConn(o));
     private static readonly Parser<char, QOp> OpWhitespace = Op.Between(SkipWhitespaces);
     private static readonly Parser<char, QConn> ConnWhitespace = Conn.Between(SkipWhitespaces);
-	private static readonly Parser<char, IQuery> Value = Num.Or(String);
-	private static readonly Parser<char, IQuery> Array = Value.Between(SkipWhitespaces)
+	  private static readonly Parser<char, IQuery> Value = Num.Or(String);
+	  private static readonly Parser<char, IQuery> Array = Value.Between(SkipWhitespaces)
 																		.Separated(Comma)
 																		.Between(LBracket, RBracket)
-																		.Select<IQuery>(els => new QArray(els));
-	private static readonly Parser<char, IQuery> Cond = from m in String1
+																		.Select<IQuery>(els => new QArray(els.ToImmutableArray()));
+	  private static readonly Parser<char, IQuery> Cond = from m in String1
 																  from o in OpWhitespace
 																  from v in Value.Or(Array)
 																  select (IQuery)new QCond(o, m, v);
-	private static readonly Parser<char, IQuery> Parenthesis = Cond.Or(Rec(() =>Expression)).Between(LParenthesis, RParenthesis);
-	private static readonly Parser<char, IQuery> Atom = Cond.Or(Parenthesis);
-	private static readonly Parser<char, IQuery> Expression = from l in Atom
-																		from c in ConnWhitespace
-																		from r in Atom
-																		select (IQuery)new QExpression(c, l, r);
-    
+    private static readonly Parser<char, IQuery> Parenthesis = Cond.Or(Rec(() =>Expression)).Between(LParenthesis, RParenthesis);
+    private static readonly Parser<char, IQuery> Atom = Cond.Or(Parenthesis);
+    private static readonly Parser<char, IQuery> Expression = from l in Atom
+                                      from c in ConnWhitespace
+                                      from r in Atom
+                                      select (IQuery)new QExpression(c, l, r);
+      
 
     
 
