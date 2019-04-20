55beedb7c84b6f72cb007efafb162a7702dac048
iwate <iwatesoftware@live.jp>
2019-04-20 17:14:00 +0900

    add space line

--- samples/Sample.Procedures/LongRunning.cs
+++ samples/Sample.Procedures/LongRunning.cs
@@ -11,12 +11,16 @@ namespace Sample.Procedures
         public Task ExecuteAsync(IContext context)
         {
             context.Logger.LogInformation("Start!");
+
             for (var i = 0; i < 10; i++)
             {
                 context.Progress.Report(10 * i);
+
                 context.Logger.LogInformation(string.Join("", Enumerable.Range(0, i).Select(_ => "=")) + ">");
+                
                 Task.Delay(TimeSpan.FromSeconds(3)).Wait(context.CancellationToken);
             }
+            
             context.Logger.LogInformation("Finish!");
 
             return Task.FromResult(0);
