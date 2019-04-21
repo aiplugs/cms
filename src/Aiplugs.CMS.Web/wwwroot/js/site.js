f762a0d6b4de12aa98e38e24a5c473e4271914d1
iwate <iwatesoftware@live.jp>
2019-04-21 18:34:46 +0900

    update elements

--- src/Aiplugs.CMS.Web/wwwroot/js/site.js
+++
@@ -1,9 +0,0 @@
-﻿(function () {
-    $(document).on('beforeAjaxSend.ic', function (_, ajaxSetting, elt) {
-        elt = elt[0];
-        if (ajaxSetting.type.toLowerCase() === 'post' && elt.name && elt.value) {
-            const prefix = (ajaxSetting.data || '').length > 0 ? '&' : '';
-            ajaxSetting.data += prefix + elt.name + '=' + encodeURIComponent(elt.value);
-        }
-    })
-}());
